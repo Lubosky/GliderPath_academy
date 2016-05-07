3a1b9ff745fb60503f5ea8cb91038700d65b24dd
Lubos Hricak <lubos@hricak.sk>
2016-05-07 15:33:07 +0200

    Updated to Rails 5.0

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '4.2.6'
+gem 'rails', '~> 5.0.0.rc1'
 
 gem 'pg', '~> 0.15'
 gem 'jbuilder', '~> 2.0'
--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,6 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -17,10 +8,8 @@ Bundler.require(*Rails.groups)
 
 module GliderPathAcademy
   class Application < Rails::Application
-
-    config.active_record.raise_in_transactional_callbacks = true
-
     config.generators do |generate|
+      generate.assets false
       generate.factory_girl false
       generate.helper false
     end
@@ -30,7 +19,7 @@ module GliderPathAcademy
     config.i18n.default_locale = :en
     I18n.config.enforce_available_locales = true
 
-    config.action_mailer.default_url_options = { :host => ENV.fetch('SMTP_1_DOMAIN', 'lvh.me:3000') }
+    config.action_mailer.default_url_options = { host: ENV.fetch('SMTP_1_DOMAIN', 'lvh.me:3000') }
     config.action_mailer.smtp_settings = {
       address: ENV.fetch('SMTP_1_ADDRESS', 'localhost'),
       port: ENV.fetch('SMTP_1_PORT', 1025).to_i,
