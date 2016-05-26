7da09747a12c7844d4903fe7ae5b813a06ebe90b
Lubos Hricak <lubos@hricak.sk>
2016-05-26 13:57:43 +0200

    DEPRECATION WARNING: You are passing an instance of ActiveRecord::Base to "exists?". Pass the id of the object by calling ".id"

--- app/controllers/concerns/downloadable.rb
+++ app/controllers/concerns/downloadable.rb
@@ -3,7 +3,7 @@ module Downloadable
 
   def send_to_user(attachment)
     file = Refile.store.get(attachment.file_id).download
-    if Upload.exists?(attachment)
+    if Upload.exists?(attachment.id)
       send_file file, filename: attachment.file_filename, type: attachment.file_content_type, disposition: :attachment
     else
       flash[:notice] = t('flash.attachments.error')
