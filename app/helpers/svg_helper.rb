494f2b6a2e495bae86152f80c53b61d1b8acf2a8
Lubos Hricak <lubos@hricak.sk>
2016-05-29 17:21:24 +0200

    Added SVG helper and default logo

---
+++ app/helpers/svg_helper.rb
@@ -0,0 +1,13 @@
+module SvgHelper
+  def inline_svg(filename, options = {})
+    assets = Rails.application.assets
+    file = assets.find_asset(filename).source.force_encoding('utf-8')
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    if options[:class].present?
+      svg['class'] = options[:class]
+    end
+    raw doc
+  end
+end
+
