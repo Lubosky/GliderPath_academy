85d2ccc4d8442ef9863be3afefc43a3bc7d87584
Lubosky <lubos@hricak.sk>
2016-08-02 13:30:13 +0200

    Course & workshop index layout + CSS update

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 ruby '2.3.0'
 
+gem 'autoprefixer-rails', '~> 6.3'
 gem 'bourbon', '~> 5.0.0.beta.6'
 gem 'braintree'
 gem 'cocoon'
--- app/helpers/progress_helper.rb
+++ app/helpers/progress_helper.rb
@@ -1,7 +1,7 @@
 module ProgressHelper
 
   def progress_for(resource, user)
-    content_tag :div, class: 'card-progress' do
+    content_tag :div, class: 'tile-progress' do
       content_tag( :div, { class: 'progress', data: { progress: "#{ resource.progress(user) }" } } ) do
         "#{ number_to_percentage(resource.progress(user), precision: 0) }"
       end
