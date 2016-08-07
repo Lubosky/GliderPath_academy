b184698bbb03bb0e03294186372da32230d12182
Lubosky <lubos@hricak.sk>
2016-08-07 08:48:49 +0200

    Order lessons by position

--- app/models/section.rb
+++ app/models/section.rb
@@ -5,7 +5,7 @@ class Section < ApplicationRecord
                        numericality: { only_integer: true, greater_than: 0 }
 
   belongs_to :course, inverse_of: :sections, foreign_key: :course_id
-  has_many :lessons, autosave: true, dependent: :destroy, inverse_of: :section
+  has_many :lessons, -> { order(position: :asc) }, autosave: true, dependent: :destroy, inverse_of: :section
 
   accepts_nested_attributes_for :lessons, reject_if: :all_blank, allow_destroy: true
 
