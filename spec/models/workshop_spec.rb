dd9b025187cf6c686efb3b4332a3ecfca02b9df0
Lubosky <lubos@hricak.sk>
2016-08-09 11:22:52 +0200

    Added tests

--- spec/models/workshop_spec.rb
+++ spec/models/workshop_spec.rb
@@ -29,4 +29,85 @@ describe Workshop, type: :model do
 
     it { is_expected.to validate_uniqueness_of(:slug) }
   end
+
+  describe '#has_attachments?' do
+    it 'returns true when the workshop has uploads' do
+      workshop = build_workshop_with_attachment
+
+      expect(workshop).to have_attachments
+    end
+
+    it 'returns false for workshops with no uploads' do
+      workshop = build_workshop
+
+      expect(workshop).not_to have_attachments
+    end
+  end
+
+  describe '#is_free?' do
+    context 'when workshop has price 0' do
+      it 'returns true' do
+        workshop = build_workshop_with_price(0)
+
+        expect(workshop.is_free?).to be_truthy
+      end
+    end
+
+    context 'when workshop has price other than 0' do
+      it 'returns true' do
+        workshop = build_workshop_with_price(9.99)
+
+        expect(workshop.is_free?).to be_falsy
+      end
+    end
+  end
+
+  describe '#watchable_for' do
+    context 'when workshop has price 0' do
+      it 'returns true' do
+        user = build_stubbed(:user)
+        workshop = build_workshop_with_price(0)
+
+        expect(workshop.watchable_for(user)).to be_truthy
+      end
+    end
+
+    context 'when user purchased the workshop' do
+      it 'returns true' do
+        user = create(:user)
+        workshop = build_workshop_with_price(9.99)
+        purchase = create(:purchase, purchasable: workshop, purchaser: user)
+
+        expect(workshop.watchable_for(user)).to be_truthy
+      end
+    end
+
+    context 'when workshop does not purchased workshop' do
+      it 'returns false' do
+        user = build_stubbed(:user)
+        workshop = build_workshop_with_price(9.99)
+
+        expect(workshop.watchable_for(user)).to be_falsy
+      end
+    end
+  end
+
+  def build_workshop
+    stub_video
+    build_stubbed(:workshop)
+  end
+
+  def build_workshop_with_price(price)
+    stub_video
+    build_stubbed(:workshop, price: price)
+  end
+
+  def build_workshop_with_attachment
+    stub_video
+    user = create(:user)
+    RequestStore.store[:current_user] = user
+    workshop = build_stubbed(:workshop, uploads_attributes:
+      [ attributes_for(:upload, :attachment) ]
+    )
+  end
 end
