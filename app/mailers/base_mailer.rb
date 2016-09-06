222387520311c5896e07d98c6f39d5dd07bed4fc
Lubosky <lubos@hricak.sk>
2016-09-06 17:42:10 +0200

    Fixed sending emails

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -7,12 +7,12 @@ class BaseMailer < ActionMailer::Base
   default template_path: Proc.new { self.class.name.gsub(/Mailer$/, '').underscore }
   prepend_view_path ['app/views/mailers']
 
-  default from: Settings.application.name, reply_to: Settings.application.mailer
+  default from: Settings.application.mailer_sender,
+          reply_to: Settings.application.mailer
 
   private
 
-    def application_logo
-      attachments.inline['GliderPath-Academy-logo.png'] = { content: File.read("#{Rails.root}/app/assets/images/logo-default.png"), mime_type: 'image/png' }
-    end
-
+  def application_logo
+    attachments.inline['GliderPath-Academy-logo.png'] = { content: File.read("#{Rails.root}/app/assets/images/logo-default.png"), mime_type: 'image/png' }
+  end
 end
