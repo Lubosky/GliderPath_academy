1249736feaf5e5e9be024d0963e4f5d8ae34fe21
Lubosky <lubos@hricak.sk>
2016-09-30 10:19:55 +0200

    Confirm user only if not already confirmed

--- app/workers/confirm_user_worker.rb
+++ app/workers/confirm_user_worker.rb
@@ -3,10 +3,12 @@ class ConfirmUserWorker
 
   def perform(user_id)
     user = User.find_by(id: user_id)
-    if user.stripe_customer_id.present?
-      user.confirm
-    else
-      user.send_confirmation_instructions
+    unless user.confirmed?
+      if user.stripe_customer_id.present?
+        user.confirm
+      else
+        user.send_confirmation_instructions
+      end
     end
   end
 end
