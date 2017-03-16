013ed6cf415db3b6bb7656908898c52af9452c81
Lubosky <lubos@hricak.sk>
2017-03-16 21:31:44 +0100

    Updated Stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Stripe.api_key = ENV['STRIPE_API_KEY'] || 'stripe_api_key'
 STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY'] || 'stripe_public_key'
-Stripe.api_version = '2016-07-06'
+Stripe.api_version = '2017-02-14'
 
 unless defined? STRIPE_JS_HOST
   STRIPE_JS_HOST = 'https://js.stripe.com'
