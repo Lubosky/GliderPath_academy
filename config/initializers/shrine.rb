4c97d91162823c00b14fb576ba5c4d5f25bf2181
Lubosky <lubos@hricak.sk>
2017-04-02 15:22:34 +0200

    Updated Shrine initializer

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -2,8 +2,8 @@ require 'shrine'
 require 'shrine/storage/file_system'
 
 Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new('/tmp/uploads/cache'),
-  store: Shrine::Storage::FileSystem.new('/tmp/uploads/store')
+  cache: Shrine::Storage::FileSystem.new(Rails.root.join('tmp'), prefix: 'uploads/cache'),
+  store: Shrine::Storage::FileSystem.new(Rails.root.join('tmp'), prefix: 'uploads/store')
 }
 
 Shrine.plugin :activerecord
