310360f993f403b1767185da9e44aede1dff43a0
Lubosky <lubos@hricak.sk>
2017-04-02 22:22:50 +0200

    Removed FileUploader from parent classes

--- app/models/concerns/uploadable.rb
+++ app/models/concerns/uploadable.rb
@@ -3,8 +3,6 @@ module Concerns
     extend ActiveSupport::Concern
 
     included do
-      include FileUploader[:file]
-
       has_many :uploads, dependent: :destroy, as: :uploadable
       has_many :uploaders, through: :uploads, as: :uploadable, foreign_key: :uploader_id, class_name: 'User'
     end
